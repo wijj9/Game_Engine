f7e042a040a0efd75af9d88aa039c5fdb17e3793
Wajih <ge47jez@tum.de>
2023-08-27 15:12:30 +0200

    first draft

--- Rock_Paper_Scissors.js
+++ Rock_Paper_Scissors.js
@@ -34,45 +34,87 @@ function playRound(playerSelection, computerSelection) {
     else if(playerSelection.toUpperCase() ==="PAPER" && computerSelection.toUpperCase() ==="ROCK") {
         return 1
     }
-    else {
-        return -2
+}
+
+
+let result = 0;
+let score_player = 0;
+let score_computer = 0;
+
+let game_over = false;
+
+const playerSelection = document.querySelectorAll("button");
+
+const Player = document.querySelector(".Player");
+const Computer = document.querySelector(".Computer");
+const Result = document.querySelector(".Result");
+
+const result_list = document.querySelector(".Result_List");
+
+const restart_button = document.createElement("button");
+
+
+function restart() {
+    result_list.removeChild(restart_button);
+    Player.textContent = `Player: 0`;
+    Computer.textContent = `Computer: 0`;
+
+    playerSelection.forEach(selection => selection.addEventListener("click", handle_selection));
+
+    result = 0;
+    score_player = 0;
+    score_computer = 0;
+    game_over = false;
+}
+
+function handle_End_Game(e) {
+
+
+    if (score_computer === 5) {
+        Result.textContent = "You lost :( ,Computer Won!";
+        game_over = true;
+    } else if (score_player === 5) {
+        Result.textContent = "You Won! Congrats";
+        game_over = true;
+    }
+
+    if(game_over) {
+
+        playerSelection.forEach(selection => selection.removeEventListener("click", handle_selection))
+
+        restart_button.textContent = "Restart Game";
+        restart_button.classList.add("restart")
+        restart_button.addEventListener("click", restart);
+        result_list.appendChild(restart_button);
     }
 }
 
 
+    function handle_selection(event) {
+
+        let selection = event.target.textContent;
+
+        let computerChoice = getComputerChoice();
+        if (selection === "Rock") {
+            result = playRound("Rock", computerChoice);
+        } else if (selection === "Paper") {
+            result = playRound("Paper", computerChoice);
+        } else if (selection === "Scissors") {
+            result = playRound("Scissors", computerChoice);
+        }
 
 
-function game() {
-    let score_player = 0
-    let score_computer = 0
-    for(let i = 0; i<5; i++) {
-        let playerSelection = prompt("Enter your play:")
-        let computerSelection = getComputerChoice()
-        console.log("Computers Choice: ",computerSelection)
-        console.log("Players Choice: ",playerSelection)
-        let result = playRound(playerSelection, computerSelection)
-        if(result === 1) {
+
+        if (result === 1) {
             score_player++
-        }
-        else if(result === -1) {
+            Player.textContent = `Player: ${score_player}`;
+        } else if (result === -1) {
             score_computer++
+            Computer.textContent = `Computer: ${score_computer}`;
         }
-        else if(result === -2){
-            console.log("Not a valid option!")
-        }
-    }
-    console.log("Player Score: ",score_player)
-    console.log("Computer Score: ",score_computer)
 
-    if(score_computer > score_player) {
-        console.log("You Lost!")
+        handle_End_Game();
     }
-    else if(score_computer < score_player) {
-        console.log("You Won!")
-    }
-    else {
-        return console.log("Draw ")
-    }
-}
- game()
+    playerSelection.forEach(selection => selection.addEventListener("click", handle_selection));
+
 
