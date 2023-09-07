f6bf69c1efbe7747ddee22e0afa44fad26ef065e
Wajih <wajihtarkhani9@gmail.com>
2023-09-07 02:54:03 +0200

    links fixed

--- Home/Library/script.js
+++ Home/Library/script.js
@@ -21,17 +21,17 @@ panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
 
 RPS.addEventListener("click", function() {
     if (RPS.classList.contains('open-active')) {
-        window.location.href = "../../Games/Rock_Paper_Scissors/index.html";
+        window.location.href = "../Games/Rock_Paper_Scissors/index.html";
     }
 });
 
 Signature.addEventListener("click", function() {
     if (Signature.classList.contains('open-active')) {
-        window.location.href = "../../Games/Signature_Generator/index.html";
+        window.location.href = "../Games/Signature_Generator/index.html";
     }
 });
 Toggle_Switch.addEventListener("click", function() {
     if (Toggle_Switch.classList.contains('open-active')) {
-        window.location.href = "../../Games/Custom_Toggle_Switch/index.html";
+        window.location.href = "../Games/Custom_Toggle_Switch/index.html";
     }
 });
