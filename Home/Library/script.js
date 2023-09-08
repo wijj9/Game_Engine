c59734db92fcdab0eedd73330ce2e4455b933339
Wajih <wajihtarkhani9@gmail.com>
2023-09-08 02:42:13 +0200

    Library performance issues fix1 + Toggle Switch

--- Home/Library/script.js
+++ Home/Library/script.js
@@ -1,9 +1,5 @@
 const panels = document.querySelectorAll('.panel');
 
-const RPS = document.getElementById("p2");
-const Signature = document.getElementById("p3");
-const Toggle_Switch = document.getElementById("p5");
-
 function toggleOpen() {
     console.log('Hello');
     this.classList.toggle('open');
@@ -19,19 +15,3 @@ function toggleActive(e) {
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
 panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
 
-RPS.addEventListener("click", function() {
-    if (RPS.classList.contains('open-active')) {
-        window.location.href = "../Games/Rock_Paper_Scissors/index.html";
-    }
-});
-
-Signature.addEventListener("click", function() {
-    if (Signature.classList.contains('open-active')) {
-        window.location.href = "../Games/Signature_Generator/index.html";
-    }
-});
-Toggle_Switch.addEventListener("click", function() {
-    if (Toggle_Switch.classList.contains('open-active')) {
-        window.location.href = "../Games/Custom_Toggle_Switch/index.html";
-    }
-});
