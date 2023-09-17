c2905137866653330853da06371c0f8ba2e4f6d5
Wajih <wajihtarkhani9@gmail.com>
2023-09-17 20:07:56 +0200

    Suggestions list added to the search bar

--- script.js
+++ script.js
@@ -43,19 +43,61 @@ const Random_Button = document.querySelector(".random");
 
 const Search_Field = document.getElementById('user_input');
 
-
+const Programs_List = [
+    { word: "Rock Paper Scissors", url: "./Home/Games/Rock_Paper_Scissors/index.html" },
+    { word: "Password Generator", url: "./Home/Games/Password_Generator/index.html" },
+    { word: "Signature Creator", url: "./Home/Games/Signature_Generator/index.html" },
+    { word: "Tic Tac Toe", url: "./Home/Games/Tic_Tac_Toe/index.html" },
+    { word: "Toggle Switch", url: "./Home/Games/Custom_Toggle_Switch/index.html" },
+    // Add more suggestions with their respective URLs
+];
 /*Variables*/
 const RPS_Word_List = ["ROCK", "PAPER","SCISSORS", "ROCKS","PAPERS","SCISSOR"]
 
 
-function search() {
-    const user_input = Search_Field.value;
-    console.log("starting...")
-    for (const word of RPS_Word_List) {
-        console.log(user_input);
-        if(user_input.toUpperCase().includes(word)) {
-            console.log("if == true...")
-            window.location.href = "./Home/Games/Rock_Paper_Scissors/index.html"
+const searchInput = document.getElementById("user_input");
+const suggestionsList = document.getElementById("suggestions-list");
+
+const suggestions = [
+    "Rock Paper Scissors",
+    "Password Generator",
+    "Signature Creator",
+    "Tic Tac Toe",
+    "Toggle Switch",
+];
+
+function displaySuggestions() {
+    const searchTerm = searchInput.value.toLowerCase();
+    const filteredSuggestions = suggestions.filter((suggestion) =>
+        suggestion.toLowerCase().includes(searchTerm)
+    );
+
+    suggestionsList.innerHTML = "";
+    filteredSuggestions.forEach((suggestion) => {
+        const suggestionItem = document.createElement("li");
+        suggestionItem.classList.add("suggestion-item");
+        suggestionItem.textContent = suggestion;
+        suggestionItem.addEventListener("click", () => {
+            search(suggestion)
+            searchInput.value = "";
+            suggestionsList.style.display = "none";
+        });
+        suggestionsList.appendChild(suggestionItem);
+    });
+
+    if (filteredSuggestions.length > 0) {
+        suggestionsList.style.display = "block";
+    } else {
+        suggestionsList.style.display = "none";
+    }
+}
+
+
+function search(input) {
+    const user_input = Search_Field.value.toUpperCase(); // Convert user input to uppercase
+    for (const suggestion of Programs_List) {
+        if(suggestion.word === input) {
+            window.location.href = suggestion.url;
             break;
         }
     }
@@ -89,5 +131,8 @@ window.addEventListener("beforeunload", function () {
     checkBox.checked = localStorage.getItem("dark-mode") === "enabled";
 });
 
+
+// Event listener for input changes
+searchInput.addEventListener("input", displaySuggestions);
 
 start();
