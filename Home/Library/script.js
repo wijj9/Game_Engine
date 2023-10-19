625795fb945a020a7490292569956fb725bd5214
Wajih <wajihtarkhani9@gmail.com>
2023-10-20 01:14:58 +0200

    Library performance issue fixed

--- Home/Library/script.js
+++ Home/Library/script.js
@@ -1,12 +1,10 @@
 const panels = document.querySelectorAll('.panel');
 
 function toggleOpen() {
-    console.log('Hello');
     this.classList.toggle('open');
 }
 
 function toggleActive(e) {
-    console.log(e.propertyName);
     if (e.propertyName.includes('flex')) {
         this.classList.toggle('open-active');
     }
